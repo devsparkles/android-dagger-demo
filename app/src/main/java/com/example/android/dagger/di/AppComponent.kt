91376264fd2d29fcada5a860f42c0967e8e77a64
Xavier Maximin <xmaximin@gmail.com>
2020-03-23 19:11:08 +0000

    Put login as a subcomponent

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,6 +1,7 @@
 package com.example.android.dagger.di
 
 import android.content.Context
+import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationComponent
 import com.example.android.dagger.settings.SettingsActivity
@@ -21,6 +22,7 @@ interface AppComponent {
 
     // Expose RegistrationComponent factory from the graph
     fun registrationComponent(): RegistrationComponent.Factory
+    fun loginComponent(): LoginComponent.Factory
 
     fun inject(activity: MainActivity)
     fun inject(activity: SettingsActivity)
